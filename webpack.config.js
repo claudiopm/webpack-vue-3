72f6fb4e54f6afc8fa48f2d676d5b8e467a245fb
claudiopm <40447468+claudiopm@users.noreply.github.com>
2023-01-28 17:15:19 -0300

    quarto

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+//Dependencias de desenvolvimento
+const path = require('path');
+
+//
+const { VueLoaderPlugin } = require('vue-loader');
+
+//separa todos os arquivos css em arquivos separados
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+//
+module.exports = {
+    entry: './src/main.js'
+}
